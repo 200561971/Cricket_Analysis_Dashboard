54bb78588d72d58ed41e6b892ef8769db3090406
200561971 <200561971@student.georgianc.on.ca>
2023-09-18 11:31:02 -0400

    moved all the csv files to data folder

--- utils/constants.py
+++ utils/constants.py
@@ -6,9 +6,9 @@ class Urls:
     player_query_url = "https://hs-consumer-api.espncricinfo.com/v1/pages/player/search"
 
 class Constants:
-    PLAYER_PROFILE_FILE_NAME = 'player_profiles.csv'
-    PLAYER_BATTING_STATS_FILE_NAME = 'batting_stats.csv'
-    PLAYER_BOWLING_STATS_FILE_NAME = 'bowling_stats.csv'
-    PLAYER_SHORT_FILE_NAME = 'players_short.csv'
-    TEAMS_FILE_NAME = 'teams.csv'
+    PLAYER_PROFILE_FILE_NAME = 'data/player_profiles.csv'
+    PLAYER_BATTING_STATS_FILE_NAME = 'data/batting_stats.csv'
+    PLAYER_BOWLING_STATS_FILE_NAME = 'data/bowling_stats.csv'
+    PLAYER_SHORT_FILE_NAME = 'data/players_short.csv'
+    TEAMS_FILE_NAME = 'data/teams.csv'
     MENS_INTERNATIONAL_TEAMS = "POPULAR MEN'S INTERNATIONAL TEAMS"
